6dcde0bdc10e351bfd986d4d5daef88ab9b312b7
Wanli <puwanli@gmail.com>
2019-01-19 20:40:59 -0600

    add array hopper 1 solution

---
+++ dynamic_programming/one_D_DP/array_hopper_1.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def canJump(self, array):
+        """
+        input: int[] array
+        return: boolean
+        """
+        # write your solution here
+
+        record = [float('Inf')] * len(array)
+
+        # if array[0] == 0:
+        #     return False
+
+        record[0] = 0
+
+        for i in range(1, len(array)):
+            for j in range(i):
+                if array[j] >= i - j and record[j] + 1 < record[i]:
+                    record[i] = record[j] + 1
+
+        return record[len(array) - 1] != float('Inf')
+
+
+if __name__ == "__main__":
+
+    example = Solution()
+
+    source = [0, 1, 2, 3]
+    print('the length of Palindrome substring: ' + str(example.canJump(source)) + '\n')
+
+    source = [1, 1, 1, 1, 1]
+    print('the length of Palindrome substring: ' + str(example.canJump(source)) + '\n')
+
+    source = [1, 2, 1, 1, 0, 1]
+    print('the length of Palindrome substring: ' + str(example.canJump(source)) + '\n')
+
+    source = [100, 0, 0, 0, 0, 0, 0]
+    print('the length of Palindrome substring: ' + str(example.canJump(source)) + '\n')
+
+    source = [0]
+    print('the length of Palindrome substring: ' + str(example.canJump(source)) + '\n')
